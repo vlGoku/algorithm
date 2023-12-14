c3b0a380e2deefebee365da7b1fa0cfb4f476df0
vlGoku <markolucic21@outlook.de>
2023-12-14 13:43:52 +0100

    add .map

--- sortieralgo/bubblesort/bubblesort.js
+++ sortieralgo/bubblesort/bubblesort.js
@@ -1,15 +1,17 @@
-function bubbleSort(arr) {
-    var n = arr.length;
-    for (var i = n - 1; i > 0; i--) {
-        for (var j = 0; j < i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                var temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+"use strict";
+function bubbleSort(array) {
+    const n = array.length;
+    for (let i = n - 1; i > 0; i--) {
+        for (let j = 0; j < i; j++) {
+            if (array[j] > array[j + 1]) {
+                const temporary = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temporary;
             }
         }
     }
-    return arr;
+    return array;
 }
-var unsortedArray = [12, 22, 1, 4, 5, 90, 100];
+const unsortedArray = [12, 22, 1, 4, 5, 90, 100];
 console.log(bubbleSort(unsortedArray));
+//# sourceMappingURL=bubblesort.js.map
