bfd8c263eebf4952288c1875625bb805d1e5cd85
vlGoku <markolucic21@outlook.de>
2023-12-15 14:04:45 +0100

    add pivot.ts

---
+++ sortieralgo/pivot/pivot.ts
@@ -0,0 +1,13 @@
+function swap (arr: number[], index1: number, index2: number){
+    const temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
+
+function pivot(arr: number[], start: number = 0, end: number = arr.length - 1){
+    let j = 0;
+    let pivotElement = arr[j];
+    for(let i = 0; i < arr.length; i++){
+
+    }
+}
