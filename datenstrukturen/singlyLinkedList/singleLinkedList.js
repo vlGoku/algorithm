dbdf994bcb2b0a975a31d6ebe9cafe156ff718a7
vlGoku <markolucic21@outlook.de>
2023-12-19 12:16:16 +0100

    add datenstrukturen folder

---
+++ datenstrukturen/singlyLinkedList/singleLinkedList.js
@@ -0,0 +1,37 @@
+"use strict";
+class ListNode {
+    constructor(value) {
+        this.next = null;
+        this.value = value;
+        this.next = null;
+    }
+}
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    append(value) {
+        const listNodeValue = new ListNode(value);
+        if (this.length === 0) {
+            this.head = listNodeValue;
+            this.tail = this.head;
+        }
+        else {
+            this.tail.next = listNodeValue;
+            this.tail = listNodeValue;
+        }
+        this.length++;
+        return this;
+    }
+}
+const myList = new SinglyLinkedList();
+myList.append(5);
+myList.append(7);
+myList.append(12);
+console.log(myList);
+/* const head = new ListNode(5);
+head.next = new ListNode(10);
+head.next.next = new ListNode(20);
+console.log(head); */ 
---
+++ datenstrukturen/singlyLinkedList/singleLinkedList.ts
@@ -0,0 +1,38 @@
+class ListNode {
+    value: number;
+    next: ListNode | null = null;
+    constructor(value: number){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    head: ListNode | null = null;
+    tail: ListNode | null = null;
+    length: number = 0;
+
+    append(value: number){
+        const listNodeValue = new ListNode(value)
+        if(this.length === 0){
+            this.head = listNodeValue
+            this.tail = this.head
+        } else {
+            this.tail!.next = listNodeValue
+            this.tail = listNodeValue
+        }
+        this.length++;
+        return this;
+    }
+}
+
+const myList = new SinglyLinkedList();
+myList.append(5);
+myList.append(7);
+myList.append(12);
+console.log(myList);
+
+/* const head = new ListNode(5);
+head.next = new ListNode(10);
+head.next.next = new ListNode(20);
+console.log(head); */
