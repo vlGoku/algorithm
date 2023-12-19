02846eccbce9fdd4e12ffd99a9c679c68f25750d
vlGoku <markolucic21@outlook.de>
2023-12-19 09:48:02 +0100

    add radixSort

--- sortieralgo/radixSort/radixSort.js
+++ sortieralgo/radixSort/radixSort.js
@@ -7,4 +7,27 @@ function digitCount(num) {
         return 1;
     return Math.floor(Math.log10(Math.abs(num)) + 1);
 }
-console.log(digitCount(10000000000023791));
+function mostDigits(num) {
+    let maxDigits = 0;
+    let tempNum = 0;
+    for (let i = 0; i < num.length; i++) {
+        tempNum = digitCount(num[i]);
+        if (tempNum > maxDigits) {
+            maxDigits = tempNum;
+        }
+    }
+    return maxDigits;
+}
+function radixSort(arr) {
+    let maxDigits = mostDigits(arr);
+    for (let i = 0; i < maxDigits; i++) {
+        const buckets = Array.from({ length: 10 }, () => []);
+        arr.forEach((num) => {
+            buckets[getDigit(num, i)].push(num);
+        });
+        arr = [].concat(...buckets);
+    }
+    return arr;
+}
+console.log(radixSort([12, 34, 541, 1000, 761, 2310, 6541, 774]));
+//# sourceMappingURL=radixSort.js.map
--- sortieralgo/radixSort/radixSort.ts
+++ sortieralgo/radixSort/radixSort.ts
@@ -7,4 +7,28 @@ function digitCount(num: number) {
   return Math.floor(Math.log10(Math.abs(num)) + 1);
 }
 
-console.log(digitCount(10000000000023791));
+function mostDigits(num: number[]): number {
+  let maxDigits = 0;
+  let tempNum = 0;
+  for (let i = 0; i < num.length; i++) {
+    tempNum = digitCount(num[i]);
+    if (tempNum > maxDigits) {
+      maxDigits = tempNum;
+    }
+  }
+  return maxDigits;
+}
+
+function radixSort(arr: number[]): number[] {
+  let maxDigits = mostDigits(arr);
+  for (let i = 0; i < maxDigits; i++) {
+    const buckets: number[][] = Array.from({ length: 10 }, () => []);
+    arr.forEach((num) => {
+      buckets[getDigit(num, i)].push(num);
+    });
+    arr = ([] as number[]).concat(...buckets);
+  }
+  return arr;
+}
+
+console.log(radixSort([12, 34, 541, 1000, 761, 2310, 6541, 774]));
